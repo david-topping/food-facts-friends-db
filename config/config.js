a4caa40bcb8358cee563fa3e3e7ca4c53969c7aa
David Topping <topping.david11@gmail.com>
2026-01-05 16:10:46 +0000

    chore: updates config

--- config/config.js
+++ config/config.js
@@ -1,22 +1,22 @@
 const path = require("path");
 
-const env = process.env.NODE_ENV || "development";
+const env = process.env.NODE_ENV || "build";
 
 require("dotenv").config({
   path: path.resolve(process.cwd(), `.env.${env}`),
 });
 
 if (env === "production") {
-  console.warn("⚠️  Running migrations in PRODUCTION");
-  console.warn(`⚠️  DB_HOST=${process.env.DB_HOST}`);
-  console.warn(`⚠️  DB_NAME=${process.env.DB_NAME}`);
+  console.warn("Running migrations in PRODUCTION");
+  console.warn(`DB_HOST=${process.env.DB_HOST}`);
+  console.warn(`DB_NAME=${process.env.DB_NAME}`);
 }
 
 if (
   env === "production" &&
   ["127.0.0.1", "localhost"].includes(process.env.DB_HOST)
 ) {
-  console.warn("⚠️  PROD database appears to be on localhost");
+  console.warn("PROD database appears to be on localhost");
 }
 
 const base = {
